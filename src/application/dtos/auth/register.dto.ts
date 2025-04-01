02b965f7c7814c0f14ef3e81452bf9e124dd68ef
Checha Valerii <chechavalera@gmail.com>
2025-04-01 17:17:03 +0300

    fix(password): change password regexp

--- src/application/dtos/auth/register.dto.ts
+++ src/application/dtos/auth/register.dto.ts
@@ -19,7 +19,7 @@ export class RegisterDto {
   @IsNotEmpty()
   @MinLength(8)
   @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_])[A-Za-z\d!@#$%^&*(),.?":{}|<>_]{8,}$/,
     {
       message:
         'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character',
--- src/core/value-objects/password.vo.ts
+++ src/core/value-objects/password.vo.ts
@@ -13,7 +13,7 @@ export class Password {
   private isValid(password: string): boolean {
     // The Password must be at least 8 characters long and include at least one uppercase letter,
     // one lowercase letter, one number, and one special character
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_])[A-Za-z\d!@#$%^&*(),.?":{}|<>_]{8,}$/;
     return passwordRegex.test(password);
   }
 
