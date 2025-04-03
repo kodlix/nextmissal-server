39d3a6707719867c0eb20b30f2dd289801cacc63
Checha Valerii <chechavalera@gmail.com>
2025-04-03 16:19:31 +0300

    feat(linters): add linters and fix errors

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'coverage/**',
+      'prisma/**',
+      '*.js',
+      '.eslintrc.js'
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: '.',
+        sourceType: 'module',
+      },
+      ecmaVersion: 2020,
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs['recommended'].rules,
+      ...prettierPlugin.configs.recommended.rules,
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          'selector': 'interface',
+          'format': ['PascalCase'],
+          'custom': {
+            'regex': '^I[A-Z]',
+            'match': true
+          }
+        }
+      ],
+      'max-len': ['error', { 'code': 120, ignoreStrings: true }],
+      'no-console': ['warn', { 'allow': ['warn', 'error'] }],
+      'no-duplicate-imports': 'error',
+      'no-multiple-empty-lines': ['error', { 'max': 1 }],
+      'curly': 'error',
+      'eqeqeq': ['error', 'always'],
+      'prefer-const': 'error',
+      'eol-last': ['error', 'always'],
+    },
+  },
+];
